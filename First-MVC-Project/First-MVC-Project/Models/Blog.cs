4804f7201813e3acfa7e800a74753c553d4adfa2
sgorkemcil <sgorkemcil@gmail.com>
2022-03-17 15:13:06 +0300

    First-mvc-project

---
+++ First-MVC-Project/First-MVC-Project/Models/Blog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace First_MVC_Project.Models
+{
+    public class Blog
+    {
+        public int ID { get; set; }
+        public string Baslik { get; set; }
+        public string Detay { get; set; }
+
+        public DateTime Tarih { get; set; }
+
+        //public Yazar Yazar{get;set;}
+
+
+
+    }
+}
---
+++ First-MVC-Project/First-MVC-Project/Models/Yazar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace First_MVC_Project.Models
+{
+    public class Yazar
+    {
+        public int ID { get; set; }
+        public string YazarAdi { get; set; }
+
+       
+
+
+
+    }
+}
